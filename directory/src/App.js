e6f9e6aef097f9703ad07ef4429a332d27c3c1d1
Niv Suroghon <nsuroghon@gmail.com>
2021-02-25 16:47:17 -0500

    base navigation functional

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Navigation from './components/navigation'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import Nav from './components/navbar'
 // import Header from "./components/Header";
@@ -7,8 +8,11 @@ import React, {Component} from "react";
 class App extends Component {
   render(){
     return(
-      <div className="container">
-        <h1>My Blog</h1>
+      <div>
+        <Navigation />
+        <div className="container">
+          <h1>My Blog</h1>
+        </div>
       </div>
     )
   }
---
+++ directory/src/components/navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Navbar  from "react-bootstrap/Navbar";
+// import Container from "react-bootstrap/Container";
+
+class Navigation extends React.Component {
+  render() {
+    return(
+      <div>Navigation</div>
+    )
+  }
+}
+   
+export default Navigation;
