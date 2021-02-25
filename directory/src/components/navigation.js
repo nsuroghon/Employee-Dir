df4c741560c3afbd506d5ec6d40e99ca5bc3da5c
Niv Suroghon <nsuroghon@gmail.com>
2021-02-25 16:52:00 -0500

    bootstrap navigation

--- directory/src/components/navigation.js
+++ directory/src/components/navigation.js
@@ -5,7 +5,9 @@ import React from "react";
 class Navigation extends React.Component {
   render() {
     return(
-      <div>Navigation</div>
+      <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">Navbar</span>
+      </nav>
     )
   }
 }
