561f34e97d1388fc9017470ef3eb18b47338b4bc
Niv Suroghon <nsuroghon@gmail.com>
2021-02-26 14:50:34 -0500

    axios api call

--- directory/src/App.js
+++ directory/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import Form from "./components/form";
 import Navigation from './components/navigation'
 import Reset from './components/reset'
+import Table from "./components/table";
 
 // import Header from "./components/Header";
 // import Section from "./components/Section";
@@ -9,14 +10,14 @@ import Reset from './components/reset'
 class App extends Component {
   render(){
     return(
-      <div>
-        <Navigation />
-        <div className="container">
-          <h1>My Blog</h1>
-        </div>
+      <div><Navigation />
         <div>
           <Form /> <Reset />
         </div>
+        <div>
+          <Table />
+        </div>
+
       </div>
     )
   }
--- directory/src/utils/api.js
+++ directory/src/utils/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/";
-const APIKEY = "AYAW-28HE-C3G1-0IAY";
 
-export default {
-  search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
+const BASEURL = "https://randomuser.me/api/?inc=picture,name,phone,email,location,dob";
+
+ const API = {
+  search: function() {
+      return axios.get(BASEURL);
   }
 };
+
+export default API;
