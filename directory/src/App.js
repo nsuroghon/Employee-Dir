c62dfcb3f67eff31e43aad50c0baf211b5943b31
Niv Suroghon <nsuroghon@gmail.com>
2021-02-26 14:55:21 -0500

    removing table component from app.js

--- directory/src/App.js
+++ directory/src/App.js
@@ -2,10 +2,7 @@ import React, {Component} from "react";
 import Form from "./components/form";
 import Navigation from './components/navigation'
 import Reset from './components/reset'
-import Table from "./components/table";
 
-// import Header from "./components/Header";
-// import Section from "./components/Section";
 
 class App extends Component {
   render(){
@@ -13,9 +10,6 @@ class App extends Component {
       <div><Navigation />
         <div>
           <Form /> <Reset />
-        </div>
-        <div>
-          <Table />
         </div>
 
       </div>
