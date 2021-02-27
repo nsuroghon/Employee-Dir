b68f6eb35f3b8ec1738739ec7c3943200bff683e
Niv Suroghon <nsuroghon@gmail.com>
2021-02-27 12:55:27 -0500

    formatted filter forms

--- directory/src/components/form.js
+++ directory/src/components/form.js
@@ -3,21 +3,24 @@ import React from "react";
 class Form extends React.Component {
     render() {
       return(
-        <div>
-            <form>
-                <label>
-                    <input type="text" name="name" value="filter by (property)" />
-                </label>
-                    <input type="submit" value="Submit" />
-            </form>
-
-            <form>
-                <label>
-                    <input type="text" name="name" value="filter by (property)" />
-                </label>
-                    <input type="submit" value="Submit" />
-            </form>
-        </div>
+          <div className="container">
+              <div className="form-container">
+                <form>
+                    <label>
+                        <input type="text" name="name" value="filter by (property)" />
+                    </label>
+                        <input type="submit" value="Submit" />
+                </form>
+              </div>
+              <div className="form-container">
+                <form>
+                    <label>
+                        <input type="text" name="name" value="filter by (property)" />
+                    </label>
+                        <input type="submit" value="Submit" />
+                </form>
+              </div>
+          </div>
       )}}
 
 export default Form;
