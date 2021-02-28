f4705b27137d77c45b2a234a1ebabde9c9dab0ba
Niv Suroghon <nsuroghon@gmail.com>
2021-02-28 15:58:05 -0500

    styling form/search bar

--- directory/src/components/Form/form.js
+++ directory/src/components/Form/form.js
@@ -1,25 +1,48 @@
 import React from "react";
-import './forms.css'
 
 class Form extends React.Component {
     render() {
       return(
-          <div className="formDiv">
-              <div>
-                <form>
-                    <label>
-                        <input type="text" name="name" value="filter by (property)" />
-                    </label>
-                </form>
-              </div>
-              <div>
-                <form>
-                    <label>
-                        <input type="text" name="name" value="filter by (property)" />
-                    </label>
-                </form>
-              </div>
-          </div>
-      )}}
+        <nav className="navbar navbar-light bg-light justify-content-center">
+        <form className="form-inline m-2">
+          <input
+            className="form-control"
+            // value={props.value}
+            name="search"
+            // onChange={props.handleInputChange}
+            type="search"
+            placeholder="Search"
+          />
+        </form>
+        <form className="form-inline m-2">
+          <input
+            className="form-control"
+            // value={props.value}
+            name="search"
+            // onChange={props.handleInputChange}
+            type="search"
+            placeholder="Search"
+          />
+        </form>
+      </nav>
+    )}}
+
+    //       <div className="formDiv">
+    //           <div>
+    //             <form>
+    //                 <label>
+    //                     <input type="text" name="name" value="filter by (property)" />
+    //                 </label>
+    //             </form>
+    //           </div>
+    //           <div>
+    //             <form>
+    //                 <label>
+    //                     <input type="text" name="name" value="filter by (property)" />
+    //                 </label>
+    //             </form>
+    //           </div>
+    //       </div>
+    //   )}}
 
 export default Form;
